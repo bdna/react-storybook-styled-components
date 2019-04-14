ff2f66f58f127313bc87fc7e804bf75b073b635a
ryan <ryanmccombe@gmail.com>
2019-04-14 22:58:53 +0100

    Config form design

--- src/components/ConfigurationForm/ConfigurationForm.js
+++ src/components/ConfigurationForm/ConfigurationForm.js
@@ -6,32 +6,69 @@ import config from "../../config";
 
 const Form = styled.form`
   background: white;
+  padding: 2em;
+`;
+
+const List = styled.ul`
+  list-style: none;
+  padding: 0;
+`;
+
+const Label = styled.label`
+  display: block;
+  font-size: 1.2em;
+  font-weight: bold;
+`;
+
+const Input = styled.input`
+  display: block;
+  width: 300px;
+  padding: 1em;
+  margin-bottom: 2em;
+`;
+
+const TextButton = styled.button`
+  border: none;
+  border-bottom: 1px dashed black;
+  outline: none;
+  background: none;
+  cursor: pointer;
 `;
 
 function ConfigurationForm(props) {
   return (
     <Form onSubmit={props.save}>
-      <label>Place Id</label>
-      <input
+      <Label>Place Id</Label>
+      <Input
         value={props.placeId}
         onChange={e => props.setPlaceId(e.target.value)}
       />
       Or select an example:
-      <ul>
+      <List>
         {config.examplePlaces.map(place => (
-          <li key={place.id} onClick={() => props.setPlaceId(place.id)}>
-            {place.name}
+          <li key={place.id}>
+            <TextButton
+              type="button"
+              onClick={() => props.setPlaceId(place.id)}
+            >
+              {place.name}
+            </TextButton>
           </li>
         ))}
-      </ul>
-      <label>Theme</label>
-      <ul>
+      </List>
+      <Label>Theme</Label>
+      <List>
         {config.availableThemes.map(theme => (
           <li key={theme.id} onClick={() => props.setTheme(theme.id)}>
+            <input
+              type="radio"
+              defaultChecked={props.theme === theme.id}
+              name="theme"
+            />{" "}
             {theme.name}
           </li>
         ))}
-      </ul>
+      </List>
       <button type="submit">Save</button>
     </Form>
   );
