73b1549432beeda7fc33d4d1ddc05f8badf56d2c
ryan <ryanmccombe@gmail.com>
2019-04-14 20:28:46 +0100

    API - google hookup & add endpoint for updating config

--- server/server.js
+++ server/server.js
@@ -1,24 +1,57 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const fs = require('fs');
+const axios = require('axios');
 
 const app = express();
 const port = 3001;
 
 app.use(cors());
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
+const API_KEY = 'AIzaSyAY1R-fKt1bT5MEZGgYaD6KE0VpVNfjE3U';
+const placeId = 'ChIJFa7vmf8IYUgR8wY6TeDnHmg'; // market
+// ChIJyRWwvfgIYUgRvgpiKzKFUik starbucks
+// ChIJcYljJ7AJYUgREnKW65tyJUw titanic
+
+app.get('/', (req, res, next) => {
 	const raw = fs.readFileSync('server/data.json');
 	const data = JSON.parse(raw);
 	
-	// TODO: make google places API call and add to data
+	const googleRequestUrl = `https://maps.googleapis.com/maps/api/place/details/json?key=${API_KEY}&placeid=${data.placeId}`;
 	
-	res.send(data)
+	axios.get(googleRequestUrl).then(googleResponse => {
+		if (!googleResponse.data.result) {
+			return res.status(400).send({ error: 'This place ID is not recognised' });
+		}
+		
+		const { reviews } = googleResponse.data.result;
+		if (!reviews || !reviews.length) {
+			return res.status(404).send({ error: 'This place has no reviews'});
+		}
+		
+		res.send({
+			...data,
+			reviews: reviews.map(review => (
+				{
+					// Generate a unique ID based on the review timestamp and author's name
+					id: `${review.time}${review.author_name}`,
+					name: review.author_name,
+					text: review.text
+				}
+			))
+		});
+	}).catch(next);
 });
 
-// NYI
 app.put('/', (req, res) => {
-	res.send('posted')
+	if (!req.body || !req.body.placeId || !req.body.theme) {
+		return res.status(400).send({ error: 'Request must contain a placeId and a theme' })
+	}
+	
+	fs.writeFileSync('server/data.json', JSON.stringify(req.body));
+	res.send(req.body);
 });
 
 app.listen(port, () => console.log(`Server listening on ${port}`));
