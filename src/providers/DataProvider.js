01bd17ba8ad1d2d86cdbeffdf6bb5212d92fee2b
ryan <ryanmccombe@gmail.com>
2019-04-14 07:11:44 +0100

    Connect DataProvider to API

--- src/providers/DataProvider.js
+++ src/providers/DataProvider.js
@@ -1,23 +1,44 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
 import ThemeProvider from './ThemeProvider';
 
 const Context = React.createContext({});
 
-const sampleData = { theme: 'green', reviews: [] };
+const API_URL = '//localhost:3001';
+
+// TODO: add localstorage for offline
+function getData() {
+	return axios.get(API_URL).then((res) => {
+		return res.data;
+	}).catch(() => {
+		return { theme: 'dark', reviews: [] };
+	})
+}
 
 class DataProvider extends Component {
-	state = {
-		...sampleData
-	};
+	state = {};
+	
+	updateData() {
+		getData().then(data => {
+			this.setState({ theme: data.theme, reviews: data.reviews })
+		});
+	}
 	
 	componentDidMount() {
-		// TODO: get data from API then assign to state.data
-		// this.setState({ sampleData });
+		this.updateData();
+		this.interval = window.setInterval(this.updateData.bind(this), 60000);
+	}
+	
+	componentWillUnmount() {
+		window.clearInterval(this.interval);
 	}
 	
 	render() {
 		const { children } = this.props;
 		const { theme, reviews } = this.state;
+		
+		if (!reviews) return null;
 		
 		return (
 			<Context.Provider value={reviews}>
@@ -38,3 +59,6 @@ export function withReviews(Component) {
 		</Context.Consumer>
 	);
 }
+
+// withTheme is available from the styled-components package
+// https://www.styled-components.com/docs/advanced#theming
